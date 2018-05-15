4868547679b697b5e1eb7941b4feb9836cbe9bba
Artiow <namednev_a@mail.ru>
2018-05-15 14:23:12 +0300

    regenerate

---
+++ src/main/java/ru/reksoft/demo/boot/ReksoftDemoApplication.java
@@ -0,0 +1,12 @@
+package ru.reksoft.demo.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReksoftDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReksoftDemoApplication.class, args);
+    }
+}

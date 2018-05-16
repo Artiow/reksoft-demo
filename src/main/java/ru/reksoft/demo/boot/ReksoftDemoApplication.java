454962682c7c0afc91d44f9611fabad35aeffb35
Artiow <namednev_a@mail.ru>
2018-05-16 11:59:24 +0300

    config fix

--- src/main/java/ru/reksoft/demo/boot/ReksoftDemoApplication.java
+++ src/main/java/ru/reksoft/demo/boot/ReksoftDemoApplication.java
@@ -2,8 +2,13 @@ package ru.reksoft.demo.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({
+        "ru.reksoft.demo.config",
+        "ru.reksoft.demo.controller"
+})
 public class ReksoftDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReksoftDemoApplication.class, args);
--- src/main/java/ru/reksoft/demo/config/WebSecurityConfig.java
+++ src/main/java/ru/reksoft/demo/config/WebSecurityConfig.java
@@ -10,9 +10,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf()
-                    .disable()
-
                 .authorizeRequests()
                     .anyRequest().permitAll();
     }

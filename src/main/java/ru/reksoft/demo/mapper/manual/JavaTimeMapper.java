545e1afff39a8538bb8f8a7ab79dddb345772dc2
Artiow <namednev_a@mail.ru>
2018-06-20 08:24:41 +0300

    deprecated methods cut out

--- src/main/java/ru/reksoft/demo/mapper/manual/JavaTimeMapper.java
+++ src/main/java/ru/reksoft/demo/mapper/manual/JavaTimeMapper.java
@@ -1,9 +1,7 @@
 package ru.reksoft.demo.mapper.manual;
 
-import org.postgresql.util.PGInterval;
 import org.springframework.stereotype.Component;
 
-import java.sql.Time;
 import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -11,26 +9,6 @@ import java.time.LocalTime;
 
 @Component
 public class JavaTimeMapper {
-
-    @Deprecated
-    public Time toTime(LocalTime localTime) {
-        return Time.valueOf(localTime);
-    }
-
-    @Deprecated
-    public LocalTime toLocalTime(Time time) {
-        return time.toLocalTime();
-    }
-
-    @Deprecated
-    public PGInterval toInterval(LocalTime localTime) {
-        return new PGInterval(0, 0, 0, localTime.getHour(), localTime.getMinute(), localTime.getSecond());
-    }
-
-    @Deprecated
-    public LocalTime toLocalTime(PGInterval pgInterval) {
-        return LocalTime.of(pgInterval.getHours(), pgInterval.getMinutes(), (int) pgInterval.getSeconds());
-    }
 
     public Timestamp toTimestamp(LocalTime localTime) {
         return Timestamp.valueOf(LocalDateTime.of(LocalDate.of(1,1,1), localTime));

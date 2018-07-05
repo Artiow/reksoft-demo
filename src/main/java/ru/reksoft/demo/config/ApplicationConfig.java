8c72dd3ec605bb859aef14a4ddd9a026fad8f941
Artiow <namednev_a@mail.ru>
2018-07-05 14:08:40 +0300

    user simple controller add

--- src/main/java/ru/reksoft/demo/config/ApplicationConfig.java
+++ src/main/java/ru/reksoft/demo/config/ApplicationConfig.java
@@ -2,14 +2,19 @@ package ru.reksoft.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -18,15 +23,25 @@ public class ApplicationConfig implements WebMvcConfigurer {
     /**
      * Swagger configuration.
      *
-     * @return Docket bean
+     * @return docket bean
      */
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        Docket docket = new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("ru.reksoft.demo.controller"))
                 .paths(PathSelectors.any())
                 .build();
+
+        return docket
+                .globalOperationParameters(Collections.singletonList(
+                        new ParameterBuilder()
+                                .name(HttpHeaders.AUTHORIZATION)
+                                .modelRef(new ModelRef("string"))
+                                .parameterType("header")
+                                .required(false)
+                                .build()
+                ));
     }
 
 
--- src/main/java/ru/reksoft/demo/controller/api/UserController.java
+++ src/main/java/ru/reksoft/demo/controller/api/UserController.java
@@ -1,10 +1,26 @@
 package ru.reksoft.demo.controller.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.reksoft.demo.service.UserService;
 
 @RestController
 @RequestMapping("${api-path.user}")
 public class UserController {
 
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @GetMapping
+    public String login(@RequestParam String login, @RequestParam String password) {
+        return userService.login(login, password);
+    }
 }

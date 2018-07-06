3099aeeca9deb5763721e874f65ba1f5108b138a
Artiow <namednev_a@mail.ru>
2018-07-06 16:38:36 +0300

    auth fix

--- src/main/java/ru/reksoft/demo/mapper/manual/security/UserDetailsMapper.java
+++ src/main/java/ru/reksoft/demo/mapper/manual/security/UserDetailsMapper.java
@@ -1,21 +1,20 @@
 package ru.reksoft.demo.mapper.manual.security;
 
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
-import java.util.Iterator;
+import java.util.Collection;
 import java.util.Map;
 
 @Component
 public class UserDetailsMapper {
 
-    private static final String SEPARATOR = ", ";
-
-    private static final ObjectMapper mapper = new ObjectMapper();
+    private static final ObjectMapper mapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
     private static final TypeReference<Map<String, Object>> mapTypeReference = new TypeReference<Map<String, Object>>() {
 
@@ -23,29 +22,98 @@ public class UserDetailsMapper {
 
 
     public Map<String, Object> toMap(UserDetails userDetails) {
-        Map<String, Object> map = mapper.convertValue(userDetails, mapTypeReference);
-        Iterator<? extends GrantedAuthority> i = userDetails.getAuthorities().iterator();
-        if (i.hasNext()) {
-            StringBuilder builder = new StringBuilder(i.next().getAuthority());
-            while (i.hasNext()) {
-                builder.append(SEPARATOR).append(i.next().getAuthority());
-            }
-
-            map.put("authorities", builder.toString());
-        }
-
-        return map;
+        return mapper.convertValue(userDetails, mapTypeReference);
     }
 
     public UserDetails toUserDetails(Map<String, Object> map) {
-        return User.builder()
-                .password((String) map.get("password"))
-                .username((String) map.get("username"))
-                .authorities(((String) map.get("authorities")).trim().split(SEPARATOR))
-                .accountExpired(!((boolean) map.get("accountNonExpired")))
-                .accountLocked(!((boolean) map.get("accountNonLocked")))
-                .credentialsExpired(!((boolean) map.get("credentialsNonExpired")))
-                .disabled(!((boolean) map.get("enabled")))
-                .build();
+        return User.withUserDetails(mapper.convertValue(map, UserDetailsImpl.class)).build();
+    }
+
+    private static class UserDetailsImpl implements UserDetails {
+
+        private String password;
+        private String username;
+        private Collection<GrantedAuthorityImpl> authorities;
+        private boolean accountNonExpired;
+        private boolean accountNonLocked;
+        private boolean credentialsNonExpired;
+        private boolean enabled;
+
+        @Override
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+        @Override
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        @Override
+        public Collection<GrantedAuthorityImpl> getAuthorities() {
+            return authorities;
+        }
+
+        public void setAuthorities(Collection<GrantedAuthorityImpl> authorities) {
+            this.authorities = authorities;
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return accountNonExpired;
+        }
+
+        public void setAccountNonExpired(boolean accountNonExpired) {
+            this.accountNonExpired = accountNonExpired;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return accountNonLocked;
+        }
+
+        public void setAccountNonLocked(boolean accountNonLocked) {
+            this.accountNonLocked = accountNonLocked;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return credentialsNonExpired;
+        }
+
+        public void setCredentialsNonExpired(boolean credentialsNonExpired) {
+            this.credentialsNonExpired = credentialsNonExpired;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return enabled;
+        }
+
+        public void setEnabled(boolean enabled) {
+            this.enabled = enabled;
+        }
+    }
+
+    private static class GrantedAuthorityImpl implements GrantedAuthority {
+
+        private String role;
+
+        @Override
+        public String getAuthority() {
+            return role;
+        }
+
+        public void setAuthority(String role) {
+            this.role = role;
+        }
     }
 }

df06a5ac5e6e33034b9d417299c6e1269d7ceb92
Artiow <namednev_a@mail.ru>
2018-07-18 13:45:19 +0300

    token service testing

--- src/test/java/ru/reksoft/demo/service/TokenServiceTests.java
+++ src/test/java/ru/reksoft/demo/service/TokenServiceTests.java
@@ -1,22 +1,42 @@
 package ru.reksoft.demo.service;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import ru.reksoft.demo.boot.ReksoftDemoApplication;
+import ru.reksoft.demo.service.security.TokenService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ReksoftDemoApplication.class)
 public class TokenServiceTests {
 
-    @Test
-    public void generate() {
+    private TokenService tokenService;
 
+    @Autowired
+    public void setTokenService(TokenService tokenService) {
+        this.tokenService = tokenService;
     }
 
-    @Test
-    public void verify() {
 
+    @Test
+    public void generateAndVerify_mapSentAndMapReturns_mapsMatched() {
+
+        // arrange
+        Map<String, Object> entryMap = new HashMap<>();
+        entryMap.put("field1", "value1");
+        entryMap.put("field2", "value2");
+
+        // act
+        Map<String, Object> resultMap = tokenService.verify(tokenService.generate(entryMap));
+
+        // assert
+        Assert.assertEquals("value1", resultMap.get("field1"));
+        Assert.assertEquals("value2", resultMap.get("field2"));
     }
 }
